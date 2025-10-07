af29107ad15aad4352e7ef0bc69fb49c865a873c
sakshamwithweb <websaksham39@gmail.com>
2025-10-07 12:31:11 +0530

    looks same but prev right ctrls, weren;t working as we had rewrited but now just wrote the AI nothing else

--- content.js
+++ content.js
@@ -1,14 +1,17 @@
 const createBtn = () => {
     const right_ctrls = document.getElementsByClassName("ytp-right-controls")[0]
-    right_ctrls.innerHTML = `<button id="edu-ai" class="ytp-button">
-    <svg width="90%" height="90%" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg">
+    const btn = document.createElement("button")
+    btn.classList.add("ytp-button")
+    btn.setAttribute("id","edu-ai")
+    btn.innerHTML = `<svg width="90%" height="90%" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg">
         <rect width="36" height="36" fill="transparent" />
         <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-family="Arial, sans-serif"
             font-size="14" fill="#fff" font-weight="bold">
             AI
         </text>
-    </svg>
-</button>` + right_ctrls.innerHTML
+    </svg>`
+    
+    right_ctrls.insertBefore(btn,right_ctrls.firstChild)
 
     const ourBtn = document.getElementById("edu-ai")
     ourBtn.addEventListener("click", () => {
