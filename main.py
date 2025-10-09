d42de513c1ef8ed0aa39b31670220041908ec2df
sakshamwithweb <websaksham39@gmail.com>
2025-10-09 12:03:57 +0530

    usiong callbacks everywhere except in is_parse..

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
-from flask import Flask, request
-import requests
+from flask import Flask, request # in request from flask, we don't use () in .json
+import requests # here in .json we use (): .json()
 import os
 from dotenv import load_dotenv
 from flask_cors import cross_origin
+import redis
+
 
 load_dotenv()
 app = Flask(__name__)
+rds = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
 headers = {"Authorization": os.getenv("API_KEY")}
 
+# Verified
+@app.route("/memories-callback", methods=["POST"])
+@cross_origin()
+def memories_callback():
+    data = request.json
+    if data['status'] == "SUCCEEDED":
+        task_id = data['taskId']
+        req = requests.get(  # Get videoNo
+            "https://api.memories.ai/serve/api/v1/get_video_ids_by_task_id",
+            headers=headers,
+            params={"task_id": task_id}
+        )
+        res = req.json()
+        if (res["success"]):
+            video_no = res["data"]["videos"][0]["video_no"]
+            rds.set(task_id, video_no) # In future you may wanna add a expiry..
+    return {"success": True}
 
+
+# Verified
 @app.route("/upload", methods=['POST'])
 @cross_origin()
 def upload():
@@ -18,7 +41,8 @@ def upload():
         "https://api.memories.ai/serve/api/v1/scraper_url",
         json={
             "video_urls": [url],
-            "unique_id": "default"
+            "unique_id": "default",
+            "callback_url": "https://unulcerous-unelating-andra.ngrok-free.dev/memories-callback",
         },
         headers=headers
     )
@@ -27,21 +51,34 @@ def upload():
     return {"success": True, "task_id": taskId}
 
 
+# Verified
 @app.route("/task", methods=['POST'])
 @cross_origin()
 def task():
-    payload = dict(request.json)
+    payload = request.json
     task_id = payload["taskId"]
-    response = requests.get(
-        "https://api.memories.ai/serve/api/v1/get_video_ids_by_task_id",
+    # We can check the db if present so send video_no with success or else success is false..
+    if rds.exists(task_id):
+        video_no = rds.get(task_id)
+        return {"success": True, "video_no": video_no}
+    return {"success": False}
+
+
+# Unverified..
+@app.route("/is_parsed", methods=['POST']) # Since there is no callback that tells us the video is parsed, we will have to do polling (from extension)
+@cross_origin()
+def is_parsed():
+    payload = dict(request.json)
+    video_no = payload["videoNo"]
+    response = requests.post(
+        "https://api.memories.ai/serve/api/v1/list_videos",
         headers=headers,
-        params={
-            "task_id": task_id,
-            "unique_id": "default"
+        json={
+            "video_no": video_no
         }
     )
-    if ((response.json())["success"]):
-        videos = (response.json())["data"]["videos"]
-        return {"success": True, "videos": videos}
+
+    if ((response.json())["success"] and (response.json())["data"]["videos"][0]["status"] == "PARSE"):
+        return {"success": True}
     else:
         return {"success": False}
