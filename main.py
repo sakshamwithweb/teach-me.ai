c33cb33e28833fe6ca097453c54b6cddd616dc30
sakshamwithweb <websaksham39@gmail.com>
2025-10-13 16:43:19 +0530

    storing uploaded video_no with its video_id to prevent dplicate videos. when get req at /upload, checking whether it exists or not then doing..

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from flask import Flask, request # in request from flask, we don't use () in .json
-import requests # here in .json we use (): .json()
+from flask import Flask, request  # in request from flask, we don't use () in .json
+import requests  # here in .json we use (): .json()
 import os
 from dotenv import load_dotenv
 from flask_cors import cross_origin
 import redis
-
+from pymongo import MongoClient
+from youtube_extractor import extract_video_id_from_url
 
 load_dotenv()
+
 app = Flask(__name__)
+
 rds = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
+mongo_client = MongoClient(os.getenv("MONGO_URI"))
+mongo_db = mongo_client[os.getenv("MONGO_DB_NAME")]
+video_collection = mongo_db["videos"]
 
 headers = {"Authorization": os.getenv("API_KEY")}
 
 # Verified
+
+
 @app.route("/memories-callback", methods=["POST"])
 @cross_origin()
 def memories_callback():
@@ -27,7 +36,12 @@ def memories_callback():
         res = req.json()
         if (res["success"]):
             video_no = res["data"]["videos"][0]["video_no"]
-            rds.set(task_id, video_no) # In future you may wanna add a expiry..
+            video_id = extract_video_id_from_url(
+                res["data"]["videos"][0]["video_url"])  # Store in DB
+            video_collection.insert_one(
+                {"video_id": video_id, "video_no": video_no})
+            # In future you may wanna add a expiry..
+            rds.set(task_id, video_no)
     return {"success": True}
 
 
@@ -37,6 +51,12 @@ def memories_callback():
 def upload():
     payload = dict(request.json)
     url = payload["url"]
+
+    video_id = extract_video_id_from_url(url)
+    video_exist_in_db = video_collection.find_one({"video_id": video_id})
+    if video_exist_in_db: # If video exist in db
+        return {"success": True, "exist": True, "video_no": video_exist_in_db["video_no"]}
+
     response = requests.post(
         "https://api.memories.ai/serve/api/v1/scraper_url",
         json={
@@ -65,7 +85,8 @@ def task():
 
 
 # Verified..
-@app.route("/is_parsed", methods=['POST']) # Since there is no callback that tells us the video is parsed, we will have to do polling (from extension)
+# Since there is no callback that tells us the video is parsed, we will have to do polling (from extension)
+@app.route("/is_parsed", methods=['POST'])
 @cross_origin()
 def is_parsed():
     payload = dict(request.json)
