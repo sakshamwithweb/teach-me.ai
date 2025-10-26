301b5ac430cd2c48b4e1400f0cf40cba470a3abb
sakshamwithweb <169585315+sakshamwithweb@users.noreply.github.com>
2025-10-26 20:15:24 +0530

    added new endpoint /user_question, that takes video_no, user question and time in sec then it generates question to know user level of understanding through memories.ai's AI then send the questions back..

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from flask_cors import cross_origin
 import redis
 from pymongo import MongoClient
 from youtube_extractor import extract_video_id_from_url
+import json
 
 load_dotenv()
 
@@ -20,8 +21,6 @@ video_collection = mongo_db["videos"]
 headers = {"Authorization": os.getenv("API_KEY")}
 
 # Verified
-
-
 @app.route("/memories-callback", methods=["POST"])
 @cross_origin()
 def memories_callback():
@@ -54,9 +53,9 @@ def upload():
 
     video_id = extract_video_id_from_url(url)
     video_exist_in_db = video_collection.find_one({"video_id": video_id})
-    if video_exist_in_db: # If video exist in db
+    if video_exist_in_db:  # If video exist in db
         return {"success": True, "exist": True, "video_no": video_exist_in_db["video_no"]}
-
+    print("CALLBACK URL:", os.getenv("CALLBACK"))
     response = requests.post(
         "https://api.memories.ai/serve/api/v1/scraper_url",
         json={
@@ -103,3 +102,31 @@ def is_parsed():
         return {"success": True}
     else:
         return {"success": False}
+
+
+@app.route("/user_question", methods=['POST'])
+@cross_origin()
+def user_question():
+    # Get video_id, question and time in second and output questions.
+    payload = dict(request.json)
+    video_no = video_collection.find_one({"video_id": payload['videoId']})['video_no']
+    question = payload["question"]
+    time_in_sec = payload["timeInSec"]
+
+    # Make questions for user based on time and question
+    prompt = f"""I am making an edtech software that simulates like being a professor in zoom through an extension, when user watch any study related video and got any doubt, it presses a button exist in bottom right, then my AI reads whole video and explains to user. After user presses the btn, we ask for where the doubt is means time and what doubt. Now we know what user had doubt and where but suppose now I generate a general answer and give to user, what if one already knows many things what I have told, what if one feels very high level of what I am telling and need detailed info? For this reason, I need to know already what user knows and what user doesn't know. Based on timestamp and doubt user have, I want you to give me 1-3 questions that I will ask user to know level of understanding user currently possess relative to what doubt it has (So as to we can look at the answer user give and know how much he knows). Give an array just, **no Markdown or any extra text**. Remember, The question must be small and lightweight such that user feel it netural rather than a mini test.\nTime: {time_in_sec} sec\nQuestion: {question}"""
+    print(prompt)
+    
+    req = requests.post(
+        "https://api.memories.ai/serve/api/v1/chat",
+        headers=headers,
+        json={
+            "video_nos": [video_no],
+            "prompt": prompt
+        },
+        stream=False
+    )
+    print(req.json()['data']['content'])
+    questions = json.loads(req.json()["data"]["content"])
+
+    return {"success": True, "questions": questions}
